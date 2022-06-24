dc0feef825ca27953bfce667dbf7c8f47df3134c
poojasasamkar1998 <poojasasamkar1998@gmail.com>
2022-06-24 09:43:15 +0530

    tff

---
+++ Area/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r, area, circum;
+            const double PI = 3.14;
+
+            Console.Write("\nEnter the radius of circle ");
+            r = Convert.ToDouble(Console.ReadLine());
+            area = PI * r * r;
+            circum = 2 * PI * r;
+            Console.WriteLine("\nThe area of circle is {0}", area);
+            //Console.WriteLine("\nThe circumference of circle is {0}", 2 * PI * r);
+            Console.WriteLine("\nThe circumference of circle is {0}", circum);
+
+        }
+    }
+}
---
+++ SwapIntg/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapInteger
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num1, num2, temp;
+            Console.Write("\nInput the First Number : ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("\nInput the Second Number : ");
+            num2 = int.Parse(Console.ReadLine());
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+            Console.Write("\nAfter Swapping : ");
+            Console.Write("\nFirst Number : " + num1);
+            Console.Write("\nSecond Number : " + num2);
+            Console.Read();
+        }
+    }
+}
